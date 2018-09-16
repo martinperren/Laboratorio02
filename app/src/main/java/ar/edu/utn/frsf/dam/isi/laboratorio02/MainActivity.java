b3d713502fc918c677da72fa65e6ce14a56bd63b
MARTIN <martinperren@gmail.com>
2018-09-16 19:13:46 -0300

    gin tonic

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
@@ -1,13 +1,47 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNuevoPedido;
+    private Button btnHistorial;
+    private Button btnListaProductos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNuevoPedido = (Button) findViewById(R.id.btnMainNuevoPedido);
+        btnNuevoPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,ListaProductos.class);
+                startActivity(i);
+            }
+        });
+
+        btnHistorial = (Button) findViewById(R.id.btnHistorialPedidos);
+        btnHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                startActivity(i);
+            }
+        });
+
+        btnListaProductos = (Button) findViewById(R.id.btnListaProductos);
+        btnListaProductos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                startActivity(i);
+            }
+        });
     }
 }
